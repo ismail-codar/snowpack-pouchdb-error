4fe2ac81dfac6b9d202dc2f696c76c2c96848bbd
ismail codar <icodar@gmail.com>
2020-06-29 20:55:23 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import PouchDB from 'pouchdb';
+import PouchAdapterMemory from 'pouchdb-adapter-memory';
+console.log(PouchAdapterMemory);
+
+document.getElementById('app').innerHTML = `
+<h1>Hello PouchDB!</h1>
+`;
